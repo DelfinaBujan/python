7905561b7cf6f4f8f1f7e6cf1e17a6bed724aac2
DelfinaBujan <dbujan@mail.austral.edu.ar>
2025-07-11 07:52:49 -0300

    Update rna_transcription.py

--- exercises/practice/rna-transcription/rna_transcription.py
+++ exercises/practice/rna-transcription/rna_transcription.py
@@ -1,2 +1,16 @@
 def to_rna(dna_strand):
+    nueva_lista=""
+    for i in dna_strand:
+        if i == "G":
+            i="C"
+        elif i =="C":
+            i="G" 
+        elif i =="T":
+            i="A"  
+        elif i=="A" :
+            i="U" 
+        nueva_lista=nueva_lista + i 
+    return nueva_lista        
     pass
+
+    
